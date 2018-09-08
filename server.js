0e5cce4736276cd2f99d7c810a1aed0846804222
eran_str <eran_str@yahoo.com>
2018-09-08 22:09:50 +0300

    init late

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const hbs = require('hbs');
+
+port = process.env.port || 3000;
+var app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs', {
+      pageTitle: 'About Page',
+      currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/',(req,res)=>{
+  res.send({
+    name: 'eran',
+    likes: ['tea', 'coffiee']
+  });
+});
+
+app.listen(port, ()=>{console.log('server is up in port 3000')});
